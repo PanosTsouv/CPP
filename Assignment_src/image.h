48a9b9434bee80900599ba760a581960bdbd7d67
PanosTsouv <Panostsouv95@gmail.com>
2019-12-19 02:42:38 +0200

    add a copy constructor and a destructor

--- Assignment_src/image.h
+++ Assignment_src/image.h
@@ -1,7 +1,6 @@
 #ifndef _IMAGE
 #define _IMAGE
 
-
 #include "array2d.h"
 #include "imageIO.h"
 #include "vec3.h"
@@ -10,12 +9,18 @@
 
 namespace image
 {
-     class Image: public math::Array2D<math::Vec3<float>>,public ImageIO
+    class Image: public math::Array2D<math::Vec3<float>>,public ImageIO
 	{
     public:
-        Image(unsigned int width = 0, unsigned int height = 0);
-        bool load(const std::string & filename, const std::string & format);
-        bool save(const std::string & filename, const std::string & format);
+        Image(unsigned int width = 0, unsigned int height = 0);                 //Declaration of default constructor
+
+        Image(const Image &src);                                                //Declaration of copy constructor
+
+        ~Image();                                                               //Declaration of destructor
+
+        bool load(const std::string & filename, const std::string & format);    //Declaration of load method,see also imageio.h for full description of this pure virtual method
+
+        bool save(const std::string & filename, const std::string & format);    //Declaration of save method,see also imageio.h for full description of this pure virtual method    
     };
 }
 #endif
