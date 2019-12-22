06cae9e9a79b518f523e2b6a3884dbfe3998111b
PanosTsouv <Panostsouv95@gmail.com>
2019-12-22 20:55:42 +0200

    add a vector to add all filters changes to check number methods and delete filters when error occur

--- Assignment_src/main.cpp
+++ Assignment_src/main.cpp
@@ -1,18 +1,16 @@
-
-
 #include <iostream>
+#include <vector>
 #include <cstring>
 #include "ppm/ppm.h"
 #include "array2d.h"
 #include "vec3.h"
 #include "image.h"
-#include "Filter.h"
+#include "filter.h"
 
 using namespace math;
 using namespace std;
 using namespace image;
 
-bool isPositiveNumber(const std::string& s); //declaration(implementation is at the end)
 bool isIntegerNumber(const std::string& s); //declaration(implementation is at the end)
 bool isNumber(const std::string& s);        //declaration(implementation is at the end)
 
@@ -23,6 +21,7 @@ int main (int argc, char *argv[]) {
     int current = 1;                                                    //cmd argument start from 1,ignore main program
     int imageWidth = 0;                                                 //imageWidth
     int imageHeight = 0;                                                //ImageHeight
+    vector<Filter*> userFilters;                                        //contains all user's filters
     image::Image *inputImage = new Image(imageWidth,imageHeight);
 
     if (imageFile.find(".") != std::string::npos)
@@ -57,20 +56,24 @@ int main (int argc, char *argv[]) {
             {
                 cout << "Filter gamma detected" << endl;
                 current++;                                              //current show next cmd argument
-				if (isPositiveNumber(argv[current]) && strtof(argv[current], NULL) >= 0.5 && strtof(argv[current], NULL) <= 2.0)        //check if gamma filter's argument is a positive float number between 0.5-2
+				if (isNumber(argv[current]) && strtof(argv[current], NULL) >= 0.5 && strtof(argv[current], NULL) <= 2.0)        //check if gamma filter's argument is a positive float number between 0.5-2
                 {
                     #ifdef DEBUG
                         cout << "Gamma's number detected" << endl;
                     #endif
-						Filter *fgamma = new FilterGamma(strtof(argv[current], NULL));  //filter gamma contains right data so we create a filter gamma
-                    *inputImage = *fgamma << *inputImage;                   //apply filter gamma to given image
-                    delete fgamma;                                          //delte filter gama
+					Filter *fgamma = new FilterGamma(strtof(argv[current], NULL));  //filter gamma contains right data so we create a filter gamma
+                    userFilters.insert(userFilters.begin(), fgamma);        //add filter to vector with all filters...add at front so the first filter will be at the end
                     current++;                                              //current show next cmd argument
                 }
                 else
-                {
+                {   
+                    while(!userFilters.empty())                             //if an error occurs we should delete filters which be created
+                    {
+                        delete userFilters.back();
+                        userFilters.pop_back();
+                    }
                     delete inputImage;                                      //delete image if an error occurs
-                    cerr << "Gamma filter dont have number or number isn't 0.5-2" << endl;     //print error message
+                    cerr << "Gamma filter dont have number or number isn't 0.5-2 or has more than one number" << endl;     //print error message
                     exit(0);
                 }
             }
@@ -80,14 +83,14 @@ int main (int argc, char *argv[]) {
                 int count = 0;                                              //count check if filter has 6 arguments
                 float linearNumbers[6];                                     //save arguments inside temp array
                 current++;                                                  //current show next cmd argument
-                while(count<6)
+                while (count < 6)
                 {
                     if (isNumber(argv[current]))                            //check if numbers of linear filter are real numbers
                     {
                         #ifdef DEBUG
                             cout << "Linear's number detected" << endl;
                         #endif
-							linearNumbers[count] = strtof(argv[current], NULL);         //put current argument inside array
+                        linearNumbers[count] = strtof(argv[current], NULL); //put current argument inside array
                         count++;                                            //increase count
                         current++;                                          //current show next cmd argument
                     }
@@ -98,9 +101,8 @@ int main (int argc, char *argv[]) {
                         exit(0);
                     }
                 }
-                Filter *flinear = new FilterLinear(Vec3<float>(linearNumbers[0],linearNumbers[1],linearNumbers[2]),Vec3<float>(linearNumbers[3],linearNumbers[4],linearNumbers[5]));  //if all data of linear filter is right create a linear filter
-                *inputImage = *flinear << *inputImage;                      //apply filter to given image
-                delete flinear;                                             //delete filter
+                Filter* flinear = new FilterLinear(Vec3<float>(linearNumbers[0], linearNumbers[1], linearNumbers[2]), Vec3<float>(linearNumbers[3], linearNumbers[4], linearNumbers[5]));  //if all data of linear filter is right create a linear filter
+                userFilters.insert(userFilters.begin(),flinear);            //add filter to vector with all filters...add at front so the first filter will be at the end
             }
             else if (strcmp(argv[current], "blur") == 0)                    //check if filter is blur
             {
@@ -111,13 +113,17 @@ int main (int argc, char *argv[]) {
                     #ifdef DEBUG
                         cout << "Blur's number detected" << endl;
                     #endif
-                    Filter *fblur = new FilterBlur(atoi(argv[current]));    //if filter blur's data is right create a filter blur
-                    *inputImage = *fblur << *inputImage;                    //apply filter bluer to given image
-                    delete fblur;                                           //delete filter
+                    FilterBlur *fblur = new FilterBlur(atoi(argv[current]));//if filter blur's data is right create a filter blur
+                    userFilters.insert(userFilters.begin(),fblur);          //add filter to vector with all filters...add at front so the first filter will be at the end
                     current++;                                              //current show next cmd argument
                 }
                 else
                 {
+                    while(!userFilters.empty())                             //if an error occurs we should delete filters which be created
+                    {
+                        delete userFilters.back();
+                        userFilters.pop_back();
+                    }
                     delete inputImage;                                      //delete image if an error occurs
                     cerr << "Blur filter dont have number or number isn't integer positive odd number" << endl; //print error message
                     exit(0);
@@ -128,11 +134,15 @@ int main (int argc, char *argv[]) {
                 cout << "Filter laplace detected" << endl;
                 current++;                                                  //current show next cmd argument
                 Filter *flaplace = new FilterLaplace();                     //if filter laplace's data is right create a filter laplace
-                *inputImage = *flaplace << *inputImage;                     //apply filter laplace to given image
-                delete flaplace;                                            //delete laplace filter
+                userFilters.insert(userFilters.begin(),flaplace);           //add filter to vector with all filters...add at front so the first filter will be at the end
             }
             else
             {
+                while(!userFilters.empty())                                 //if an error occurs we should delete filters which be created
+                {
+                    delete userFilters.back();
+                    userFilters.pop_back();
+                }
                 delete inputImage;                                          //delete image if an error occurs
                 cerr << "Wrong filter's name or filter's name is missing" << endl;  //print error message
                 exit(0);
@@ -140,6 +150,11 @@ int main (int argc, char *argv[]) {
         }
         else
         {
+            while(!userFilters.empty())                                     //if an error occurs we should delete filters which be created
+            {
+                delete userFilters.back();
+                userFilters.pop_back();
+            }
             delete inputImage;                                              //delete image if an error occurs
             cerr << "All filters should have -f or previous filter has more numbers as argument" << endl;   //print error message
             exit(0);
@@ -152,30 +167,23 @@ int main (int argc, char *argv[]) {
         exit(0);
     }
 
+    while(!userFilters.empty())                                             //use a filter and delete it until vector is empty
+    {
+        *inputImage = *userFilters.back() << *inputImage;
+        delete userFilters.back();
+        userFilters.pop_back();
+    }
+
     string partA = imageFile.substr(0, imageFile.find_last_of('/')+1);      //find the last / so we can skip path of file
     string partB = imageFile.substr(imageFile.find('/')+1, imageFile.size());   
     if((*inputImage).save(partA + "filtered_" + partB,"ppm"))               //check save of image and add "filtered" at the start of image file  
     {
         cout << "Save was successful" <<endl;
-    }              
+    }          
     delete inputImage;                                                     //delete image
 }//end of main
 
-bool isPositiveNumber(const string& s)                                          
-{
-    bool flag = false;
-    string::const_iterator it = s.begin();
-    while (it != s.end())                                                   //check all chars if contain at least a number
-    {
-        if(isdigit(*it)){
-            flag = true;
-        }
-        it++;
-    }
-    it = s.begin();
-    while (it != s.end() && isdigit(*it) || (*it == '.' && s.find_last_of('.') == s.find_first_of('.'))) ++it;  //check all chars for '.' and if number has only one time '.'
-    return !s.empty() && it == s.end() && flag == true;
-}
+
 
 bool isIntegerNumber(const string& s)
 {
@@ -186,17 +194,29 @@ bool isIntegerNumber(const string& s)
 
 bool isNumber(const string& s)
 {
-    bool flag = false;
-    string::const_iterator it = s.begin();
-    while (it != s.end())                                                   //check all chars if contain at least a number
-    {  
-        if(isdigit(*it)){
-            flag = true;
-        }
-        it++;
+    if (s.find_first_not_of("1234567890.-") != string::npos)                                            //check if s contains theses chars
+    {
+        return false;
     }
-    it = s.begin();
-     //check all chars for '.','-' and if number has only one time '.','-' and '-' is at position 0
-    while (it != s.end() && isdigit(*it) || (*it == '.' && s.find_last_of('.') == s.find_first_of('.')) || (*it == '-' && s.find_last_of('-') == 0 && s.find_first_of('-')==0)) ++it;
-    return !s.empty() && it == s.end() && flag == true;
+    if (s.find_last_of('.') != s.find_first_of('.'))                                                    //check if s contains char '.' one time 
+    {
+        return false;
+    }
+    if (s.find_last_of('-') != s.find_first_of('-'))                                                    //check if s contains char '-' one time 
+    {
+        return false;
+    }
+    if (s.find_first_of('-') != 0 && s.find('-') != std::string::npos)                                  //check if s contains 2 chars and are '-' and '.' 
+    {
+        return false;
+    }
+    if (s.length() == 2 && s.find('-') != std::string::npos && s.find('.') != std::string::npos)        //check if s contains only 1 char and is '.'
+    {
+        return false;
+    }
+    if (s.length() == 1 && (s.find('-') != std::string::npos || s.find('.') != std::string::npos))      //check if s contains only 1 char and is '-' 
+    {
+        return false;
+    }
+    return true;                                                                                        //if all statements above are true return true
 }
